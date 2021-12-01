fb12e52d4efe4cdb584e187015aae508911cb233
Jumper775 <78500318+jumpers775@users.noreply.github.com>
2021-11-30 20:46:42 -0500

    Initial Commit

---
+++ Downloader/DownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloaderApp.swift
+//  Downloader
+//
+//  Created by Luca Mignatti on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct DownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
